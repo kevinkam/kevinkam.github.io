a29ae459c26910188c99bf47b8c3d205d5faa7a1
Kevin Kam <k040691@icloud.com>
2018-09-26 18:26:06 +0800

    updated packages

--- src/js/components/Portfolio/styled.ts
+++ src/js/components/Portfolio/styled.ts
@@ -1,6 +1,7 @@
+import styled from 'styled-components'
 import { Container } from "../styled"
 
-export const Wrapper = Container.extend`
+export const Wrapper = styled(Container)`
   img {
     background-color: rgb(7, 70, 138);
     padding-bottom: 20px;
